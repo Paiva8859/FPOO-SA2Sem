cf318325924786d9d070a609b9a0463e7ffc6a40
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2023-09-11 14:59:27 -0300

    11/09/23

---
+++ Calculadoras/src/CalculadoraCombustivel.java
@@ -0,0 +1,82 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculadoraCombustivel extends JFrame {
+    private JTextField rendimentoField;
+    private JTextField quilometragemField;
+    private JTextField precoField;
+    private JLabel resultadoLabel;
+
+    public CalculadoraCombustivel() {
+        setTitle("Calculadora de Combustível");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 200);
+
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        JLabel rendimentoLabel = new JLabel("Rendimento do veiculo (km/l):");
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.anchor = GridBagConstraints.WEST;
+        panel.add(rendimentoLabel, constraints);
+
+        rendimentoField = new JTextField(10);
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        panel.add(rendimentoField, constraints);
+
+        JLabel quilometragemLabel = new JLabel("Distancia a percorrer (km):");
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        panel.add(quilometragemLabel, constraints);
+
+        quilometragemField = new JTextField(10);
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        panel.add(quilometragemField, constraints);
+
+        JLabel precoLabel = new JLabel("Preço do combustível por litro (R$/l):");
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        panel.add(precoLabel, constraints);
+
+        precoField = new JTextField(10);
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        panel.add(precoField, constraints);
+
+        JButton calcularButton = new JButton("Calcular");
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        panel.add(calcularButton, constraints);
+
+        resultadoLabel = new JLabel("Custo do combustivel: ");
+        constraints.gridx = 0;
+        constraints.gridy = 4;
+        constraints.gridwidth = 2;
+        panel.add(resultadoLabel, constraints);
+
+        calcularButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double rendimento = Double.parseDouble(rendimentoField.getText());
+                    double quilometragem = Double.parseDouble(quilometragemField.getText());
+                    double preco = Double.parseDouble(precoField.getText());
+
+                    double custo = (quilometragem / rendimento) * preco;
+                    resultadoLabel.setText("Custo estimado da viagem: R$" + String.format("%.2f", custo));
+                } catch (NumberFormatException ex) {
+                    resultadoLabel.setText("Valores invalidos ou não informados.");
+                }
+            }
+        });
+
+        add(panel);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # FPOO-SA2Sem
+
+Calculadora de custo de combustível:
+    Responsável: Gabriel Aguilar Paiva
+    Layout aplicado: Foi utilizado o GridBag para organizar os elementos no frame, pois utiliza a posição em grid x e y para determinar a posição dos elementos na tela
+    Funcionalidade: Foi criada uma ação executada ao clicar no botão de calcular, que lê as informações nos JLabels e executa os cálculos, exibindo o resultado das operações em uma Label de resultado já existente.
+
+Calculadora Padrão:
+    Responsável: Guilherme Gama Brum
+    Layout aplicado: GridLayout, pois é uma ferramenta simples para organizar elementos de forma retangular e responsiva.
+    Funcionalidade: os numeros e operações digitados são inseridos no visor (Uma JLabel) e ao clicar no sinal de igual (=) uma ação que executa os calculos com os números e operações inseridas, exibindo o resultado no mesmo visor. O visor pode ser utilizado para adicionar numeros e operações manualmente, sem o uso dos botões, assim como apagar dados no visor.
